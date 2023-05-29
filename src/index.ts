ff1d8d52a63a29cf288ef7b1e77c1a09afab726a
Bogdan Barbu <45259752+bogdanbeniaminb@users.noreply.github.com>
2023-05-29 13:57:22 +0300

    more

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { Merger } from './merger/Merger';
 export { formatJson } from './formatter/formatJson';
+export { Comparer } from './merger/Comparer';
+export { ArrayMerger } from './merger/ArrayMerger';
